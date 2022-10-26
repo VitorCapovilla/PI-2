bf2a87e31c42cbfce6a4e1a701a5dc187baf264e
IsabellaM-S <msousa.isa@gmail.com>
2022-10-26 19:24:38 -0300

    alteração js gerarBilhete

--- js/gerarBilhete.js
+++ js/gerarBilhete.js
@@ -1,3 +1,9 @@
-function imgEscolhida() {
-  document.getElementById("imgB").innerHTML = "<img src='img/Unico.png'>";
+function UnicoEscolhido() {
+  document.getElementById("imgB").innerHTML = "<img src='img/Unico.png' width='250'>";
+}
+
+function gerar()
+{
+  var resp = document.getElementById('resp');
+  resp.innerHTML = 10 * (Math.random()) + 1;
 }
