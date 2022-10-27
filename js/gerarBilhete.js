03bc3e17b8f4a37a2f2aadf58c3836b42a37c613
IsabellaM-S <msousa.isa@gmail.com>
2022-10-27 14:02:14 -0300

    funções js gerar ID

--- js/gerarBilhete.js
+++ js/gerarBilhete.js
@@ -1,9 +1,24 @@
-function UnicoEscolhido() {
-  document.getElementById("imgB").innerHTML = "<img src='img/Unico.png' width='250'>";
-}
-
 function gerar()
 {
-  var resp = document.getElementById('resp');
-  resp.innerHTML = 10 * (Math.random()) + 1;
+  document.getElementById('frase').innerHTML = 'O ID do seu Bilhete é: ';
+  document.getElementById('resp').innerHTML = 10 * (Math.random()) + 1;
+
+  //desabilitar botao após primeiro click
+    document.getElementById("id-consultar").disabled = true;
 }
+
+document.getElementById("id-consultar").disabled = true; //Desabilitado
+
+var select = document.getElementById("bilhetes");
+
+document.getElementById("bilhetes").addEventListener("change", function(event){
+
+  var conteudo = select.options[select.selectedIndex].value;
+
+  if (conteudo == ("selecione")) {
+    document.getElementById("id-consultar").disabled = true; //Habilitado
+  }else{
+    document.getElementById("id-consultar").disabled = false;
+  }
+});
+
--- js/navbar.js
+++
@@ -1,5 +0,0 @@
-menu = document.querySelector(".menu");
-menu.onclick = function() {
-    navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("active")
-}
