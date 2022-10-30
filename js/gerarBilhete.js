9c727c01a52bf58f7dcf16dad9a4e76c07c67f9a
IsabellaM-S <msousa.isa@gmail.com>
2022-10-30 13:02:44 -0300

    alert adicionado no js Gerar Bilhete

--- js/gerarBilhete.js
+++ js/gerarBilhete.js
@@ -1,10 +1,12 @@
 function gerar()
 {
   document.getElementById('frase').innerHTML = 'O ID do seu Bilhete é: ';
-  document.getElementById('resp').innerHTML = 10 * (Math.random()) + 1;
-
+  let id = document.getElementById('resp').innerHTML = 10 * (Math.random()) + 1;
+  
   //desabilitar botao após primeiro click
-    document.getElementById("id-consultar").disabled = true;
+  document.getElementById("id-consultar").disabled = true;
+
+  alert("Bilhete gerado com sucesso! Clique em 'OK' para verifficar seu ID. ");
 }
 
 document.getElementById("id-consultar").disabled = true; //Desabilitado
@@ -12,13 +14,15 @@ document.getElementById("id-consultar").disabled = true; //Desabilitado
 var select = document.getElementById("bilhetes");
 
 document.getElementById("bilhetes").addEventListener("change", function(event){
-
+  
   var conteudo = select.options[select.selectedIndex].value;
-
+  
   if (conteudo == ("selecione")) {
-    document.getElementById("id-consultar").disabled = true; //Habilitado
+    document.getElementById("id-consultar").disabled = true; //Desabilitado
   }else{
-    document.getElementById("id-consultar").disabled = false;
+    document.getElementById("id-consultar").disabled = false; //Habilitado
   }
 });
+
+
 
