00e4395896fae752710231b715dbb74e77148399
IsabellaM-S <msousa.isa@gmail.com>
2022-11-09 11:55:13 -0300

    navbar home e colunas recarga bilhete ajustadas

--- js/recarregaBilhete.js
+++ js/recarregaBilhete.js
@@ -25,35 +25,77 @@ window.onclick = function(event) {
   }
 }
 
-//SELECIONAR TIPO RECARGA
-
-var unico = document.getElementById("unico");
-var duplo = document.getElementById("duplo");
-var semanal = document.getElementById("semanal");
-var mensal = document.getElementById("mensal");
-
-if(unico.checked==true) {
-  document.getElementById('preco').innerHTML = 'R$ 4,00';
-  document.getElementById('frase').innerHTML = 'O Bilhete unico pode ser usado quantas vezes você desejar durante um período de 40 minutos que começa a ser contado a partir da primeira utilização do Bilhete.';
-}
-else if (duplo.checked==true) {
-  document.getElementById('preco').innerHTML = 'R$ 7,50';
-  document.getElementById('frase').innerHTML = 'O Bilhete duplo pode ser usado em quantos transportes você desejar, durante um período de 40 minutos, iniciando a contagem a partir da primeira utilização. Você pode repetir o uso mais uma vez, com as mesmas regras. ';
-}
-else if (semanal.checked==true) {
-  document.getElementById('preco').innerHTML = 'R$ 25,70';
-  document.getElementById('frase').innerHTML = 'O Bilhete semanal pode ser utilizado em quantos transportes você desejar durante um período de 7 dias. A contagem inicia a partir do horário do primeiro uso.';
-}
-else if (mensal.checked==true) {
-  document.getElementById('preco').innerHTML = 'R$ 90,00';
-  document.getElementById('frase').innerHTML = 'O Bilhete mensal pode ser utilizado em quantos transportes você desejar durante um período de 30 dias. A contagem inicia a partir do horário do primeiro uso.';
-}
-
 //CONFIRMAÇÃO RECARGA
-function confirma() {
-  var id = document.getElementById("idBilhete").value;
+ function confirma() {
+   var id = document.getElementById("idBilhete").value;
 
-  if(id!=null) 
-    alert("Recarga efetuada com sucesso!");
+   if(id!=null) 
+     alert("Recarga efetuada com sucesso!");
 
 }
+
+
+// Lista com tipos de recarga
+const tipos = ['Bilhete Único', 'Bilhete Duplo', 'Bilhete Semanal', 'Bilhete Mensal'];
+
+  // gerar grupo de radio       
+const group = document.querySelector("#group");
+group.innerHTML = tipos.map((tipo) => `<div>
+          <label for="${tipo}"><input type="radio" name="tipo" value="${tipo}" id="${tipo}">&nbsp;${tipo}</label>
+    </div>`).join(' ');
+
+  // funcao para escutar mudança de radio
+const radioButtons = document.querySelectorAll('input[name="tipo"]');
+for(const radioButton of radioButtons){
+    radioButton.addEventListener('change', showSelected);
+}        
+
+function showSelected(e) {
+    console.log(e);
+    if (this.value == 'Bilhete Único') {
+        document.querySelector('#preco').innerText = `R$ 4,00`;
+        document.querySelector('#explicacao').innerText = ' Bilhete unico pode ser usado quantas vezes você desejar durante um período de 40 minutos que começa a ser contado a partir da primeira utilização do Bilhete.';
+    }
+
+    else if (this.value == 'Bilhete Duplo') {
+      document.querySelector('#preco').innerText = 'R$ 7,50';
+      document.querySelector('#explicacao').innerText = 'O Bilhete duplo pode ser usado em quantos transportes você desejar, durante um período de 40 minutos, iniciando a contagem a partir da primeira utilização. Você pode repetir o uso mais uma vez, com as mesmas regras. ';
+    }
+
+    else if (this.value == 'Bilhete Semanal') {
+      document.querySelector('#preco').innerText = 'R$ 25,70';
+      document.querySelector('#explicacao').innerText = 'O Bilhete semanal pode ser utilizado em quantos transportes você desejar durante um período de 7 dias. A contagem inicia a partir do horário do primeiro uso.';
+    }
+
+    else if (this.value == 'Bilhete Mensal') {
+      document.querySelector('#preco').innerText = 'R$ 90,00';
+      document.querySelector('#explicacao').innerText = 'O Bilhete mensal pode ser utilizado em quantos transportes você desejar durante um período de 30 dias. A contagem inicia a partir do horário do primeiro uso.';
+    }
+}
+
+
+//mostrar formulario
+
+id = document.getElementById('idBilhete').value;
+
+// if (email_box.value == '') {
+//   alert ("Campo vazio");
+// } else {
+//   alert("Redirecionando")
+// }
+
+
+btnVerifica = document.getElementById('verificaId');
+
+if (!id)
+  btnVerifica.disabled = false;
+else
+  btnVerifica.disabled = true;
+
+btnVerifica.addEventListener("click", function() {
+  formulario = document.getElementById('formulario');
+
+  formulario.classList.toggle ('hide');
+
+  document.getElementById("verificaId").disabled = true;
+});
