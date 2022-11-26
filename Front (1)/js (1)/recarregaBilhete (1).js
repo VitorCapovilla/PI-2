a66666348521c0507c9fa96a9f211f146224c1ed
IsabellaM-S <msousa.isa@gmail.com>
2022-11-26 16:18:25 -0300

    mudando arquivos

---
+++ Front (1)/js (1)/recarregaBilhete (1).js
@@ -0,0 +1,173 @@
+//AJUDA
+var ajuda = document.getElementById("containerAjuda"); // Pegar ajuda/botão
+
+
+var botaoAjuda = document.getElementById("botaoAjuda"); // Pegar o botão para abrir
+
+
+var span = document.getElementsByClassName("fechar")[0]; // Visualizar o X
+
+// Abrir ajuda, quando clicar no botão
+botaoAjuda.onclick = function() {
+  ajuda.style.display = "block";
+}
+
+// Fechar ajuda ao clicar no X
+span.onclick = function() {
+  ajuda.style.display = "none";
+}
+
+// Fechar ajuda se clicar fora
+window.onclick = function(event) {
+  if (event.target == ajuda) {
+    ajuda.style.display = "none";
+  }
+}
+
+
+// Lista com tipos de recarga
+const tipos = ['Bilhete Único', 'Bilhete Duplo', 'Bilhete Semanal', 'Bilhete Mensal'];
+
+  // gerar grupo de radio       
+const group = document.querySelector("#group");
+group.innerHTML = tipos.map((tipo) => `<div>
+          <label for="${tipo}"><input type="radio" name="tipo" value="${tipo}" id="${tipo}">&nbsp;${tipo}</label>
+    </div>`).join(' ');
+
+  // funcao para escutar mudança de radio
+const radioButtons = document.querySelectorAll('input[name="tipo"]');
+for(const radioButton of radioButtons){
+    radioButton.addEventListener('change', showSelected);
+}        
+
+function showSelected(e) {
+    console.log(e);
+    if (this.value == 'Bilhete Único') {
+        document.querySelector('#preco').innerText = `R$ 4,00`;
+        document.querySelector('#explicacao').innerText = ' Bilhete unico pode ser usado quantas vezes você desejar durante um período de 40 minutos que começa a ser contado a partir da primeira utilização do Bilhete.';
+    }
+
+    else if (this.value == 'Bilhete Duplo') {
+      document.querySelector('#preco').innerText = 'R$ 7,50';
+      document.querySelector('#explicacao').innerText = 'O Bilhete duplo pode ser usado em quantos transportes você desejar, durante um período de 40 minutos, iniciando a contagem a partir da primeira utilização. Você pode repetir o uso mais uma vez, com as mesmas regras. ';
+    }
+
+    else if (this.value == 'Bilhete Semanal') {
+      document.querySelector('#preco').innerText = 'R$ 25,70';
+      document.querySelector('#explicacao').innerText = 'O Bilhete semanal pode ser utilizado em quantos transportes você desejar durante um período de 7 dias. A contagem inicia a partir do horário do primeiro uso.';
+    }
+
+    else if (this.value == 'Bilhete Mensal') {
+      document.querySelector('#preco').innerText = 'R$ 90,00';
+      document.querySelector('#explicacao').innerText = 'O Bilhete mensal pode ser utilizado em quantos transportes você desejar durante um período de 30 dias. A contagem inicia a partir do horário do primeiro uso.';
+    }
+}
+
+//mostrar opçoes
+id = document.getElementById('number-code').value;
+
+
+btnVerifica = document.getElementById('verificaId');
+
+if (!id)
+  btnVerifica.disabled = false;
+else
+  btnVerifica.disabled = true;
+
+btnVerifica.addEventListener("click", function() {
+  formulario = document.getElementById('formulario');
+
+  formulario.classList.toggle ('hide');
+
+  document.getElementById("verificaId").disabled = true;
+});
+
+
+//aqui
+const botao = document.querySelectorAll('.botao-recarga');
+const code = document.getElementById('number-code');
+console.log(botao);
+
+botao.forEach(element => {
+    element.addEventListener('click', (e) => {
+        var id = e.target.getAttribute('id');
+
+        localStorage.setItem("codigo", code.value);
+
+        if (id === 'botao-unico-recarga') localStorage.setItem("tipo", "Bilhete Único");
+        else if (id === 'botao-duplo-recarga') localStorage.setItem("tipo", "Bilhete Duplo");
+        else if (id === 'botao-semanal-recarga') localStorage.setItem("tipo", "Bilhete Semanal");
+        else {                                   localStorage.setItem("tipo", "Bilhete Mensal");
+        }
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = String(today.getFullYear());
+        var hours = String(today.getHours()).padStart(2, '0');
+        var minutes = String(today.getMinutes()).padStart(2, '0');
+        var seconds = String(today.getSeconds()).padStart(2, '0');
+
+        today = dd + '/' + mm + '/' + yyyy + ' - ' + hours + ':' + minutes + ':' + seconds;
+
+        localStorage.setItem("data", today);
+
+        let codigo = localStorage.getItem("codigo");
+        let tipo = localStorage.getItem("tipo");
+        let data = today;
+
+        let objBilhete = { codigo: parseInt(codigo), tipo: tipo, data: data };
+        let url = `http://localhost:3000/recarga`
+
+        let res = axios.post(url, objBilhete)
+            .then(response => {
+                if (response.data) {
+                    const msg = new Comunicado(
+                        response.data.codigo,
+                        response.data.tipo,
+                        response.data.data
+                    );
+
+                    alert(msg.get());
+                }
+            })
+            .catch(error => {
+
+                if (error.response) {
+                    const msg = new Comunicado(
+                        error.response.data.codigo,
+                        error.response.data.tipo,
+                        error.response.data.data
+                    );
+
+                    alert(msg.get());
+                }
+            })
+
+        function Comunicado(codigo, tipo, data) {
+            this.codigo = codigo;
+            this.tipo = tipo;
+            this.data = data;
+
+            this.get = function () {
+                return (
+                    this.codigo + " \n " +
+                    this.tipo + " \n " +
+                    this.data
+                );
+            }
+        }
+    });
+
+
+});
+
+/*
+function unico() {
+let condigounico = document.getElementById('botao-unico').value
+console.log('respostarecargaBilhete', condigounico)
+
+axios.post("http://localhost:3000/recargaBilhete", {codigo: condigounico,tipo:"unico"}).then(( res) => {console.log("resposta do res do relatorio: ", res)})
+
+}
+*/
---
+++ api (1).js
@@ -0,0 +1,238 @@
+function banco_de_dados() {
+    process.env.ORA_SDTZ = "UTC-3";
+
+    this.getConexao = async function () {
+        if (global.conexao) return global.conexao;
+
+        const oracledb = require('oracledb');
+        const dbConfig = require('./dbconfig.js');
+
+        try {
+            global.conexao = await oracledb.getConnection({
+                user: "ebd1es971",
+                password: "Awkyv7",
+                connectionString: "CEATUDB02"
+            });
+        }
+        catch (err) {
+            console.log("Falha na conexão com o Oracle DB");
+            process.exit(1);
+        }
+
+        console.log("Oracle DB conectado");
+
+        return global.conexao;
+    }
+
+    this.estrutureSe = async function () {
+        try {
+            const conexao = await this.getConexao();
+
+      
+            const bdBilhetes = 'CREATE TABLE BILHETESS (NUMERO_BILHETE INTEGER NOT NULL PRIMARY KEY, DATA_CRIACAO varchar(100) NOT NULL)';
+            const bdRecarga = 'CREATE TABLE RECARGAS (ID_RECARGA NUMBER NOT NULL PRIMARY KEY, NUMERO_BILHETE INTEGER NOT NULL, TIPO VARCHAR(50) NOT NULL,' +
+                'DATA_RECARGA VARCHAR2 (100) NOT NULL, CONSTRAINT FK_RECARGAS_BILHETESS FOREIGN KEY (NUMERO_BILHETE) REFERENCES BILHETESS(NUMERO_BILHETE))';
+            
+            await conexao.execute(bdBilhetes);
+            await conexao.execute(bdRecarga);
+
+        }
+        catch (erro) { console.log("Tabela já existe"); } // se a já existe, ignora e toca em frente
+    }
+
+}
+
+function Bilhetes(bd) {
+    this.bd = bd;
+
+    this.inclua = async function (bilhete) {
+        try {
+            const conexao = await this.bd.getConexao();
+
+            const insercao = "INSERT INTO BILHETESS (NUMERO_BILHETE, DATA_CRIACAO) VALUES (:0, :1)";
+
+            const dados = [bilhete.codigo, bilhete.data];
+
+            await conexao.execute(insercao, dados);
+
+            const commit = "COMMIT";
+            await conexao.execute(commit);
+
+        }
+        catch (erro) {
+            console.log("Erro ao tentar executar o insert na tabela BilhetesS");
+        }
+    }
+
+    this.recarga = async function (bilhete) {
+        try {
+            const conexao = await this.bd.getConexao();
+
+            const insercao = "INSERT INTO RECARGAS (ID_RECARGA, NUMERO_BILHETE, TIPO, DATA_RECARGA) VALUES (ID_RECARGA_S.nextval,:0, :1, :2)";
+
+            const dados = [bilhete.codigo, bilhete.tipo, bilhete.data];
+
+            await conexao.execute(insercao, dados);
+
+            const commit = "COMMIT";
+            await conexao.execute(commit);
+
+        }
+        catch (erro) {
+            console.log("Erro ao tentar executar o insert na tabela recarga");
+        }
+    }
+
+    this.selectUm = async function (codigo) {
+        const conexao = await this.bd.getConexao();
+
+        const sql = "SELECT NUMERO_BILHETE FROM BILHETESS WHERE NUMERO_BILHETE=:0";
+        const dados = [codigo];
+        ret = await conexao.execute(sql, dados);
+
+        return ret;
+    }
+}
+
+function Bilhete(codigo, tipo, data) {
+    this.codigo = codigo;
+    this.tipo = tipo;
+    this.data = data;
+}
+
+function Comunicado(codigo, tipo, mensagem) {
+    this.codigo = codigo;
+    this.tipo = tipo;
+    this.mensagem = mensagem;
+}
+
+function middleWareGlobal(req, res, next) {
+    console.time('Requisição'); // marca o início da requisição
+    console.log('Método: ' + req.method + '; URL: ' + req.url); // retorna qual o método e url foi chamada
+
+    next(); // função que chama as próximas ações
+
+    console.log('Finalizou'); // será chamado após a requisição ser concluída
+
+    console.timeEnd('Requisição'); // marca o fim da requisição
+}
+
+async function recarregar(req, res) {
+    try {
+        const bilhete = new Bilhete(req.body.codigo, req.body.tipo, req.body.data);
+
+        ret = await global.bilhetes.selectUm(bilhete.codigo);
+
+        console.log(ret.rows[0]);
+
+        if (ret.rows[0] != undefined) {
+            await global.bilhetes.recarga(bilhete);
+            const sucesso = new Comunicado(
+                'Número do bilhete: ' + bilhete.codigo,
+                'Tipo do Bilhete: ' + bilhete.tipo,
+                'O bilhete foi gerado com sucesso',
+                //mensg
+            );
+            return res.status(201).json(sucesso);
+        } else {
+            const erro2 = new Comunicado(
+                'LJE',
+                'Bilhete inexistente',
+                'Não há bilhete cadastrado'
+            );
+            return res.status(409).json(erro2);
+        }
+    }
+    catch (erro) {
+        console.log(erro);
+        const erro2 = new Comunicado(
+            'LJE',
+            'Bilhete inexistente',
+            'Não há bilhete cadastrado'
+        );
+
+        return res.status(409).json(erro2);
+    }
+}
+
+
+async function inclusao(req, res) {
+    const bilhete = new Bilhete(req.body.codigo, req.body.tipo, req.body.data);
+
+    try {
+        const mensg = await global.bilhetes.inclua(bilhete);
+
+        const sucesso = new Comunicado(
+            'Bilhete gerado com sucesso',
+
+        );
+
+        console.log(mensg);
+        return res.status(201).json(sucesso);
+    }
+    catch (erro) {
+        console.log(erro);
+        const erro2 = new Comunicado(
+            'Bilhete em uso'
+        );
+
+        return res.status(409).json(erro2);
+    }
+}
+
+
+async function ativarServidor() {
+    const bodyParser = require("body-parser");
+    const express = require("express");
+    const app = express();
+    const path = require("path");
+    const cors = require("cors");
+    const { threadId } = require("worker_threads");
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.engine("html", require("ejs").renderFile);
+    app.set("view engine", "html");
+    app.use(cors());
+    app.use(express.json());
+    app.use("/Front", express.static(path.join(__dirname, "Front")));
+
+    const bd = new banco_de_dados();
+    await bd.estrutureSe();
+
+    global.bilhetes = new Bilhetes(bd);
+
+
+    app.use(middleWareGlobal);
+
+
+    app.get("/", function (req, res) {
+        console.log(__dirname);
+        res.render(__dirname + "Front/index.html");
+    });
+
+    app.get("/bilhete.html", function (req, res) {
+        res.render(__dirname + "/gerarBilhete.html");
+    });
+
+    app.get("/gerarBilhete", function (req, res) {
+        res.render(__dirname + "/gerarBilhete.html");
+    });
+
+    app.get("/recarga", function (req, res) {
+        res.render(__dirname + "/recarga.html");
+    });
+
+
+
+
+    app.post('/gerarBilhete', inclusao);
+
+    app.post('/recargaBilhete', recarregar);
+
+    app.listen(3000, function () {
+        console.log("Servidor rodando na porta 3000");
+    });
+}
+
+ativarServidor();
